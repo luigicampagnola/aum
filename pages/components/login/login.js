8c1940f6c4779f74bf0befdf8d85142e642f5047
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2022-03-07 21:09:50 -0600

    project

---
+++ pages/components/login/login.js
@@ -0,0 +1,28 @@
+import styles from "./login.module.css";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <h1>Member Login</h1>
+        <p>Email</p>
+        <input className={styles.input}></input>
+        <p>Password</p>
+        <input className={styles.input}></input>
+        <button className={styles.button}>Login</button>
+        <p className={styles.forgotpw}>Forgot password?</p>
+        <div
+          style={{
+            borderTop: "1px solid #c4c4c4",
+            marginLeft: 20,
+            marginRight: 20,
+          }}
+        ></div>
+        <Link href="/posts/register/register">
+          <button className={styles.createbutton}>Create new account</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
